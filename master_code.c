bc3a21cc38eecb03a62f99c0f675886cb145bca2
mohammedtiti <128482348+mohammedtiti@users.noreply.github.com>
2024-12-28 12:57:02 +0200

    Update master_code.c

--- master_code.c
+++ master_code.c
@@ -4,12 +4,18 @@
 #include <math.h>
 
 #define MAX_LINE_MEMIN 1048576
-#define LINE_LENGTH 9
+#define LINE_LENGTH 9  //line is a string
 #define MAX_LINES 1024
 #define NUMBER_CORES 4
 #define NUMBER_BLOCKS 64
 #define DSRAM_SIZE 256
 #define NUMBER_REGS 16
+
+void fetch(int i);
+void decode(int i);
+void execute(int i);
+void mem(int i);
+void write_back(int i);
 
 typedef struct {
     int pc_pipe;
@@ -24,41 +30,73 @@ typedef struct {
     int data; // data from memory 
 }temp_reg;
 
+typedef struct {
+    int arr[NUMBER_CORES];  // Array to store queue elements
+    int front;          // Index of the front of the queue
+    int rear;           // Index of the rear of the queue
+    int size;           // Current size of the queue
+} Queue;
+
+// Function to initialize the queue
+void initializeQueue(Queue* q) {
+    q->front = 0;
+    q->rear = -1;
+    q->size = 0;
+}
+
+// Function to check if the queue is full
+int isFull(Queue* q) {
+    return q->size == NUMBER_CORES;
+}
+
+// Function to check if the queue is empty
+int isEmpty(Queue* q) {
+    return q->size == 0;
+}
+
+// Function to add an element to the queue (enqueue)
+void enqueue(Queue* q, int value) {
+    if (isFull(q)) {
+    } else {
+        // Circularly update rear
+        q->rear = (q->rear + 1) % NUMBER_CORES;
+        q->arr[q->rear] = value;
+        q->size++;
+    }
+}
+
+// Function to remove an element from the queue (dequeue)
+int dequeue(Queue* q) {
+    if (isEmpty(q)) {
+        return -1;  // Indicating empty queue
+    } else {
+        int dequeuedValue = q->arr[q->front];
+        // Circularly update front
+        q->front = (q->front + 1) % NUMBER_CORES;
+        q->size--;
+        return dequeuedValue;
+    }
+}
+
+int bus_counter;
+int counter_limit;
+int flushed[NUMBER_CORES];
 char Imem0[MAX_LINES][LINE_LENGTH];
 char Imem1[MAX_LINES][LINE_LENGTH];
 char Imem2[MAX_LINES][LINE_LENGTH];
 char Imem3[MAX_LINES][LINE_LENGTH];
 char memout[MAX_LINE_MEMIN][LINE_LENGTH];
-int regs0[NUMBER_REGS];
-int regs1[NUMBER_REGS];
-int regs2[NUMBER_REGS];
-int regs3[NUMBER_REGS];
+int regs[NUMBER_CORES][NUMBER_REGS];
 temp_reg pipe_regs[NUMBER_CORES][8]; /*the registers of the pipeline (old and new for all the cores)*/
-char Dsram0[DSRAM_SIZE][LINE_LENGTH];
-char Dsram1[DSRAM_SIZE][LINE_LENGTH];
-char Dsram2[DSRAM_SIZE][LINE_LENGTH];
-char Dsram3[DSRAM_SIZE][LINE_LENGTH];
-char Tsram0[NUMBER_BLOCKS][LINE_LENGTH];
-char Tsram1[NUMBER_BLOCKS][LINE_LENGTH];
-char Tsram2[NUMBER_BLOCKS][LINE_LENGTH];
-char Tsram3[NUMBER_BLOCKS][LINE_LENGTH];
-int dirty_array0[NUMBER_BLOCKS]; /*if dirty_array0[i] == 1 then the i'th block in Dsram is dirty */
-int dirty_array1[NUMBER_BLOCKS]; 
-int dirty_array2[NUMBER_BLOCKS]; 
-int dirty_array3[NUMBER_BLOCKS]; 
-int tag_array0[NUMBER_CORES];
-int tag_array1[NUMBER_CORES];
-int tag_array2[NUMBER_CORES];
-int tag_array3[NUMBER_CORES];
-int pipeline0[5];    /*pipeline0[0] is the pc of the command that is in fetch in core0
-                      pipeline0[1] is the pc of the command that is in decode in core0
-                      pipeline0[2] is the pc of the command that is in excute in core0
-                      pipeline0[3] is the pc of the command that is in mem in core0
-                      pipeline0[4] is the pc of the command that is in write back in core0
+char Dsram[NUMBER_CORES][DSRAM_SIZE][LINE_LENGTH];
+char Tsram[NUMBER_CORES][NUMBER_BLOCKS][15];
+//int tag_array0[NUMBER_CORES][NUMBER_CORES];
+/*int pipeline[NUMBER_CORES][5];    /*pipeline[i][0] is the pc of the command that is in fetch in core i
+                      pipeline[i][1] is the pc of the command that is in decode in core i
+                      pipeline[i][2] is the pc of the command that is in excute in core i
+                      pipeline[i][3] is the pc of the command that is in mem in core i
+                      pipeline[i][4] is the pc of the command that is in write back in core i
                       if pipeline0[i] == -1 then there is nothing in it*/
-int pipeline1[5];
-int pipeline2[5];
-int pipeline3[5];
 int cycles[NUMBER_CORES];       /* cycles[i] is the number of the current cycle in core[i] */
 int instructions[NUMBER_CORES]; /* instructions[i] is the number of the instructions done in core[i] */
 int read_hit[NUMBER_CORES];     /* read_hit[i] is the number of read_hit in the Dsram of core[i] */
@@ -71,7 +109,7 @@ int pc[NUMBER_CORES];
 
 // char* filename,   char ***array,   int*;
 void main(int argc, char *argv[]){
-    int i, c;
+    int i, c, k;
     if (argc != 28){
         printf("invalid input");
         exit(0);
@@ -109,72 +147,49 @@ void main(int argc, char *argv[]){
         for(c=0; c<8; c++){
             memset(&pipe_regs[i][c], 0, sizeof(pipe_regs[i][c]));
         }
-    }
-    for(i=0; i<NUMBER_REGS; i++){
-        regs0[i] = 0;
-        regs1[i] = 0;
-        regs2[i] = 0;
-        regs3[i] = 0;
-    }
-
-    for(i=0; i<DSRAM_SIZE; i++){
-        for(c=0; c<LINE_LENGTH; c++){
-            if (c == (LINE_LENGTH-1)){
-                Dsram0[i][c] = '\0';
-                Dsram1[i][c] = '\0';
-                Dsram2[i][c] = '\0';
-                Dsram3[i][c] = '\0';
-            }
-            else{
-                Dsram0[i][c] = '0';
-                Dsram1[i][c] = '0';
-                Dsram2[i][c] = '0';
-                Dsram3[i][c] = '0';
-            }
+        for(k=0; k<NUMBER_REGS; k++){
+            regs[i][k] = 0;
+        }
+        
+        for(c=0; c<DSRAM_SIZE; c++){
+            for(k=0; k<LINE_LENGTH; k++){
+                if (k == (LINE_LENGTH-1)){
+                    Dsram[i][c][k] = '\0';
+                }
+                else{
+                    Dsram[i][c][k] = '0';
+                }
             }
         }
 
-    for(i=0; i<NUMBER_BLOCKS; i++){
-        for(c=0; c<LINE_LENGTH; c++){
-            if (c == (LINE_LENGTH-1)){
-                Tsram0[i][c] = '\0';
-                Tsram1[i][c] = '\0';
-                Tsram2[i][c] = '\0';
-                Tsram3[i][c] = '\0';
-            }
-            else{
-                Tsram0[i][c] = '0';
-                Tsram1[i][c] = '0';
-                Tsram2[i][c] = '0';
-                Tsram3[i][c] = '0';
+        for(c=0; c<NUMBER_BLOCKS; c++){
+            for(k=0; k<LINE_LENGTH; k++){
+                if (k == (LINE_LENGTH-1)){
+                    Tsram[i][c][k] = '\0';
+                }
+                else{
+                    Tsram[i][c][k] = '0';
+                }
             }
         }
-    }
 
-    for(i=0; i<NUMBER_BLOCKS; i++){
-        dirty_array0[i] = 0;
-    }
+        /*for(c=0; c<5; c++){
+            pipeline[i][c] = -1;
+        }*/
 
-    for(i=0; i<5; i++){
-        pipeline0[i] = -1;
-        pipeline1[i] = -1;
-        pipeline2[i] = -1;
-        pipeline3[i] = -1;
+        cycles[i] = 0;
+        instructions[i] = 0;
+        read_hit[i] = 0;
+        write_hit[i] = 0;
+        read_miss[i] = 0;
+        write_miss[i] = 0;
+        decode_stall[i] = 0;
+        mem_stall[i] = 0;
+        pc[i] = 0;
+        flushed[i] = 0;
     }
-
-    for(i=0; i<NUMBER_CORES;i++){
-    cycles[i] = 0;
-    instructions[i] = 0;
-    read_hit[i] = 0;
-    write_hit[i] = 0;
-    read_miss[i] = 0;
-    write_miss[i] = 0;
-    decode_stall[i] = 0;
-    mem_stall[i] = 0;
-    pc[i] = 0;
-    }
+    bus_counter = 0;
+    counter_limit = 0;
 
     // here we start after init all the global variables and the file names
 }
-
-
